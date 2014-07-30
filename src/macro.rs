70f5c42f874e33b3be973760ee26341ea5f3f212
Tom Jakubowski <tom@crystae.net>
2014-07-30 01:38:41 -0700

    Initial commit
    parses "primitive" JSON values

---
+++ src/macro.rs
@@ -0,0 +1,77 @@
+#![crate_name="json_macros"]
+#![crate_type="dylib"]
+#![feature(phase, plugin_registrar, macro_rules, quote)]
+
+#[phase(plugin, link)] extern crate log;
+extern crate rustc;
+extern crate syntax;
+extern crate serialize;
+
+use std::gc::Gc;
+use rustc::plugin::Registry;
+
+use syntax::ast;
+use syntax::codemap;
+use syntax::ext::base::{ExtCtxt, MacResult, MacExpr, DummyResult};
+use syntax::parse::token;
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+    reg.register_macro("json", expand);
+}
+
+fn expand(cx: &mut ExtCtxt, sp: codemap::Span, tts: &[ast::TokenTree]) -> Box<MacResult> {
+    debug!("JSON token tree {}", tts);
+    let tt = tts.get(0).expect("FIXME"); // FIXME
+    let expr = match tt_to_expr(cx, sp, tt) {
+        Some(e) => e,
+        None => return DummyResult::expr(sp)
+    };
+    MacExpr::new(expr)
+}
+
+fn tt_to_expr(cx: &mut ExtCtxt, sp: codemap::Span,
+              tt: &ast::TokenTree) -> Option<Gc<ast::Expr>> {
+    match *tt {
+        ast::TTTok(sp, ref tok) => token_to_expr(cx, sp, tok),
+        _ => {
+            cx.span_err(sp, "something something FIXME");
+            None
+        }
+    }
+}
+
+fn token_to_expr(cx: &mut ExtCtxt, sp: codemap::Span,
+                 tok: &token::Token) -> Option<Gc<ast::Expr>> {
+    use std::from_str::FromStr;
+    match *tok {
+        token::LIT_STR(ref n) => {
+            let s = n.as_str();
+            Some(quote_expr!(cx, {
+                ::serialize::json::String($s.to_string())
+            }))
+        }
+        token::LIT_INTEGER(ref n) => {
+            let s = n.as_str();
+            let n: i64 = FromStr::from_str(s).unwrap();
+            Some(quote_expr!(cx, {
+                ::serialize::json::Number($n as f64)
+            }))
+        }
+        token::IDENT(ref n, false) if n.as_str() == "null" => {
+            Some(quote_expr!(cx, {
+                ::serialize::json::Null
+            }))
+        }
+        ref t @ token::IDENT(..) if token::is_keyword(token::keywords::True, t) => {
+            Some(quote_expr!(cx, { ::serialize::json::Boolean(true) }))
+        }
+        ref t @ token::IDENT(..) if token::is_keyword(token::keywords::False, t) => {
+            Some(quote_expr!(cx, { ::serialize::json::Boolean(false) }))
+        }
+        _ => {
+            cx.span_err(sp, "couldn't interpret token as JSON");
+            None
+        }
+    }
+}
---
+++ tests/tests.rs
@@ -0,0 +1,25 @@
+#![feature(phase)]
+#[phase(plugin)] extern crate json_macros;
+extern crate serialize;
+
+#[test]
+fn test_string_lit() {
+    assert_eq!(json!("foo").as_string(), Some("foo"));
+}
+
+#[test]
+fn test_num_lit() {
+    assert_eq!(json!(1234).as_number(), Some(1234f64));
+    // FIXME: assert_eq!(json!(12345.6789).as_number(), Some(12345.6789f64));
+}
+
+#[test]
+fn test_null_lit() {
+    assert!(json!(null).is_null());
+}
+
+#[test]
+fn test_bool_lit() {
+    assert_eq!(json!(true).as_boolean(), Some(true));
+    assert_eq!(json!(false).as_boolean(), Some(false));
+}
