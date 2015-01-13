949cdeefd69517e62ea192fb0ec40df90a115524
Tom Jakubowski <tom@crystae.net>
2015-01-12 23:31:17 -0800

    minimal reproduction

--- examples/kitchen_sink.rs
+++ examples/kitchen_sink.rs
@@ -4,15 +4,6 @@
 extern crate "rustc-serialize" as rustc_serialize;
 
 pub fn main() {
-    let x = 123i32;
-    println!("{}", json!({ // object literal
-        "foo": "foooooo", // string literal keys and values
-        "bar": [true, null, 123, 123.4], // array, boolean, null, numeric literals
-        "quux": { // nest as deeply as you like
-            "a": [1, 2, 3, 4],
-            "b": { "a": null },
-            "c": false
-        },
-        "waldo": (192 - x) // wrap in parens to splice ToJson expressions directly
-    }).pretty().to_string());
+    let _x = json!();
+    println!("{}", _x.pretty().to_string());
 }
